6a363a7513267e408b45e823070f020686da6551
Braintelligence <Braintelligence@users.noreply.github.com>
2018-08-28 11:53:08 +0200

    Create test_docker.sh
    Looks like needed for Travis

---
+++ tests/test_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# this is a very simple script that tests the docker configuration for JanuStats
+# it is meant to be run from the root directory of the repository, eg:
+# sh tests/test_docker.sh
+
+# install test requirements
+pip install -r requirements/local.txt
+
+# create a cache directory
+mkdir -p .cache/docker
+cd .cache/docker
+
+# run the project's tests
+docker-compose -f local.yml run django python manage.py pytest
+
+# return non-zero status code if there are migrations that have not been created
+docker-compose -f local.yml run django python manage.py makemigrations --dry-run --check || { echo "ERROR: there were changes in the models, but migration listed above have not been created and are not saved in version control"; exit 1; }
+
+# Test support for translations
+docker-compose -f local.yml run django python manage.py makemessages
